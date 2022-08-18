4da4dbadad7289aaf651606dfb37d3f9d8b0b261
maryam <marmaraymansh2017@gmail.com>
2022-08-18 16:56:21 +0300

    done c.w 7
    done some bonus

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
+function BMI (weight , height) {
 
+    let result = weight / (height * height);
+    return result;
+
+}
+
+console.log( BMI ());
+
+function Status (bmi){
+
+    if (bmi < 18.5 ){
+        return " لديك نقص في الوزن 😮";
+    } else if (bmi >= 18.5 && bmi <25 ) {
+        return "وزنك صحي✨😻" ;
+    } else { 
+        return "لديك زيادة في الوزن 😱🙈";
+    }
+
+}
+
+function calculate () {
+
+    let weight = document.getElementById("weight").value;
+
+    let height = document.getElementById("height").value;
+
+    let bmi = BMI (weight , height);
+
+    let desc =  Status (bmi);
+
+    let div = document.getElementById('result');
+
+    div.innerText = bmi + " == " + desc;
+
+
+
+}
